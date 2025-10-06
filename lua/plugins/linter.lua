219aa6a7e0869b44865ce1627c530e111eb4b2ed
Tetsuyuki Okuzono <fe.snow1@gmail.com>
2025-10-06 16:22:43 +0700

    Add linter plugin specification
    Configure nvim-lint for code linting

---
+++ lua/plugins/linter.lua
@@ -0,0 +1,38 @@
+return {
+  "mfussenegger/nvim-lint",
+  config = function()
+    local lint = require("lint")
+
+    lint.linters_by_ft = {
+      javascript = { "eslint_d" },
+      typescript = { "eslint_d" },
+      javascriptreact = { "eslint_d" },
+      typescriptreact = { "eslint_d" },
+    }
+
+    lint.linters.eslint_d = {
+      cmd = "eslint_d",
+      args = {
+        "--format",
+        "json",
+        "--stdin",
+        "--stdin-filename",
+        function()
+          return vim.api.nvim_buf_get_name(0)
+        end,
+      },
+      stream = "stdout",
+      ignore_exitcode = true,
+      parser = require("lint.parser").from_errorformat("%f:%l:%c: %m", {
+        source = "eslint_d",
+        severity = vim.diagnostic.severity.WARN,
+      }),
+    }
+
+    vim.api.nvim_create_autocmd({ "BufWritePost" }, {
+      callback = function()
+        lint.try_lint()
+      end,
+    })
+  end,
+}
