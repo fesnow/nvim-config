4d72777b545c4c3d1ec1be1eca15d5f57adc6f08
Tetsuyuki Okuzono <fe.snow1@gmail.com>
2025-10-06 16:22:14 +0700

    Add lazy.nvim plugin manager configuration
    Configure lazy.nvim with UI settings and plugin loading

---
+++ lua/config/lazy.lua
@@ -0,0 +1,52 @@
+-- Bootstrap lazy.nvim
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+  local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+  local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+  if vim.v.shell_error ~= 0 then
+    vim.api.nvim_echo({
+      { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
+      { out, "WarningMsg" },
+      { "\nPress any key to exit..." },
+    }, true, {})
+    vim.fn.getchar()
+    os.exit(1)
+  end
+end
+vim.opt.rtp:prepend(lazypath)
+
+-- Make sure to setup `mapleader` and `maplocalleader` before
+-- loading lazy.nvim so that mappings are correct.
+-- This is also a good place to setup other settings (vim.opt)
+vim.g.mapleader = " "
+vim.g.maplocalleader = "\\"
+
+-- Setup lazy.nvim
+require("lazy").setup({
+  spec = {
+    -- import your plugins
+    { import = "plugins" },
+  },
+  -- Configure any other settings here. See the documentation for more details.
+  -- colorscheme that will be used when installing plugins.
+  install = { colorscheme = { "habamax" } },
+  -- automatically check for plugin updates
+  checker = { 
+    enabled = true,
+    notify = false, -- don't notify about updates
+    frequency = 3600, -- check every hour
+  },
+  change_detection = {
+    enabled = true,
+    notify = false, -- don't notify about config changes
+  },
+})
+
+-- Auto-update plugins when updates are available
+vim.api.nvim_create_autocmd("User", {
+  pattern = "LazyCheck",
+  callback = function()
+    local lazy = require("lazy")
+    lazy.update({ show = false })
+  end,
+})
